ba81267091912a1c4724cef72807b9dc648b64fb
hjmoons <gywhd408@naver.com>
2023-01-27 16:22:54 +0900

    카프카 정보를 환경변수로 불러오도록 수정 2

--- src/main/java/org/wc/WordPipeline.java
+++ src/main/java/org/wc/WordPipeline.java
@@ -4,7 +4,7 @@ import java.io.File;
 
 public class WordPipeline {
     public static void main(String[] args) {
-        File file = new File("/");
+        File file = new File(System.getenv("FILE_PATH"));
         String topic = System.getenv("KAFKA_TOPIC");
         String bootstrap = System.getenv("KAFKA_SERVER");
         PipeThread pipeThread = new PipeThread(file, topic, bootstrap);
